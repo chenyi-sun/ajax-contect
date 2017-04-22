4dd69c6d9e7906d7fb446380e3f6ae4e5f442462
Irischenyi <chenyi4as@163.com>
2017-04-22 16:59:07 +0800

    all-2

--- ajax.js
+++ ajax.js
@@ -2,8 +2,12 @@ function ajax(opts){
   var defaults = {
     method: 'GET',
     url: '',
+    data: '',
+    async: true,
     success: function() {},
-    error: function() {},
+    error: function() {
+      console.log('有错误');
+    },
   };
   for(var key in opts){
     defaults[key] = opts[key];
@@ -12,15 +16,21 @@ function ajax(opts){
   //if window.XMLHttpRequest  code for IE7+, Firefox, Chrome, Opera, Safari
   //else if code for IE6, IE5
   var oXhr = window.XMLHttpRequest ? new XMLHttpRequest():new ActiveXObject("Microsoft.XMLHTTP");
-  oXhr.open(defaults.method ,defaults.url, true);
-  oXhr.send();
+
+  if(defaults.method.toUpperCase() == 'POST'){
+    oXhr.open(defaults.method ,defaults.url, defaults.async);
+    oXhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+    oXhr.send(defaults.data);
+  }
+  else if(defaults.method.toUpperCase() == 'GET'){
+    oXhr.open(defaults.method, defaults.url + '?' + defaults.data, defaults.async);
+    
+    oXhr.send();
+  }
   oXhr.onreadystatechange=function()
   {
     if (oXhr.readyState==4 && oXhr.status==200){
       defaults.success.call(oXhr,oXhr.responseText);
-    }
-    else{
-      defaults.error();
     }
   }
 }
